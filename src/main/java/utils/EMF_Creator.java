c429143f11e19b159d2f88e09f3bb93ddbe0d796
Mads <cph-mh752@cphbusiness.dk>
2019-11-15 14:06:47 +0100

    update

--- src/main/java/utils/EMF_Creator.java
+++ src/main/java/utils/EMF_Creator.java
@@ -118,7 +118,7 @@ public class EMF_Creator {
         if (isDeployed) {
             user = System.getenv("USER");
             pw = System.getenv("PW");
-            connection_str = System.getenv("CONNECTION_CA3");
+            connection_str = System.getenv("CONNECTION_CA3_MIN");
         }
         /*
         On your server in /opt/tomcat/bin/setenv.sh   add the following WITH YOUR OWN VALUES
